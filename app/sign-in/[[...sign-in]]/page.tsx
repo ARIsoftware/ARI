4845a24ee6fe03472dad41179493ecfaf886038c
Noam Eppel <noam@morpheus.network>
2025-07-21 20:08:39 -0400

    Name Updates

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -13,7 +13,7 @@ export default function SignInPage() {
     <div className="min-h-screen bg-gray-50 flex flex-col">
       {/* Top Bar */}
       <div className="h-[35px] bg-black w-full relative z-50 flex items-center justify-center">
-        <span className={`text-white font-medium ${dmSans.className}`}>ARI-2</span>
+        <span className={`text-white font-medium ${dmSans.className}`}>ARI</span>
       </div>
 
       {/* Login Form */}
