6d436ec36942e2bc48f5be7d69eb2d511c067b75
Noam Eppel <noam@morpheus.network>
2025-08-01 14:44:38 -0400

    Manifest topbar hide

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -12,7 +12,7 @@ export default function SignUpPage() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
       {/* Top Bar */}
-      <div className="h-[35px] bg-black w-full relative z-50 flex items-center justify-center">
+      <div className="topbar h-[35px] bg-black w-full relative z-50 flex items-center justify-center">
         <span className={`text-white font-medium ${dmSans.className}`}>ARI</span>
       </div>
 
