d506d6745a56b52bd9f253550d6efd18ea320fad
Noam Eppel <653153+noameppel@users.noreply.github.com>
2026-04-16 00:05:35 -0400

    Remove bible-study database schema

--- lib/db/schema/schema.ts
+++ lib/db/schema/schema.ts
@@ -9,7 +9,6 @@
  */
 
 export * from './core-schema'
-export * from '@/modules/bible-study/database/schema'
 export * from '@/modules/brainstorm/database/schema'
 export * from '@/modules/module-template/database/schema'
 export * from '@/modules/music-player/database/schema'
